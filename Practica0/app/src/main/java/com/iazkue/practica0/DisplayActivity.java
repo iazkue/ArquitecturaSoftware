23b0f1a601c3c79109ea2c1418b7703cd10b1cd5
Iazkue <iazkue@ni.eus>
2025-05-08 03:22:10 +0200

    Practica 0

--- Practica0/app/src/main/java/com/iazkue/practica0/DisplayActivity.java
+++ Practica0/app/src/main/java/com/iazkue/practica0/DisplayActivity.java
@@ -1,6 +1,10 @@
 package com.iazkue.practica0;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,24 @@ public class DisplayActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String name = bundle.getString("name");
+        TextView textView =((TextView) findViewById(R.id.textViewDisplay));
+        textView.setTextSize(20);
+        textView.setText(name);
+    }
+
+
+    public void saveName(View view) {
+        Bundle bundle = new Bundle();
+        EditText editText = (EditText) findViewById(R.id.textViewDisplay);
+        String name = editText.getText().toString();
+        bundle.putString("name", name);
+        Intent intent = new Intent();
+        intent.putExtras(bundle);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
--- Practica0/app/src/main/java/com/iazkue/practica0/MainActivity.java
+++ Practica0/app/src/main/java/com/iazkue/practica0/MainActivity.java
@@ -1,6 +1,10 @@
 package com.iazkue.practica0;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    public void displayMessage(View view) {
+        TextView textView =((TextView) findViewById(R.id.textViewMainDisplay));
+        String name = textView.getText().toString();
+        Bundle bundle = new Bundle();
+        bundle.putString("name", name);
+        Intent intent = new Intent(this, DisplayActivity.class);
+        intent.putExtras(bundle);
+        startActivityForResult(intent, 1); // Request Code = 1
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            String name= bundle.getString("name");
+            TextView textView =((TextView) findViewById(R.id.textViewMainDisplay));
+            textView.setText(name);
+        }
     }
 }
